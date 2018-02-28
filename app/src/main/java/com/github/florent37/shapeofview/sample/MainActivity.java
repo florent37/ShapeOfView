b5b138594d6a39a6418d59a92ac5746d28f9773c
Nick Chykin <chickinnick@gmail.com>
2018-02-28 12:35:24 +0200

    mapbox integration

--- app/src/main/java/com/github/florent37/shapeofview/sample/MainActivity.java
+++ app/src/main/java/com/github/florent37/shapeofview/sample/MainActivity.java
@@ -3,8 +3,10 @@ package com.github.florent37.shapeofview.sample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
